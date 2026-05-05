637416e1e3573b5075401ebb9bcb497ddbd01154
Shankar123165 <shankarvaddi2025@gmail.com>
2026-05-05 08:41:49 +0000

    Initial commit

---
+++ monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "----- System Monitoring Project -----"
+echo "System Uptime:"
+uptime
+
+echo "Disk Usage:"
+df -h
+
+echo "Memory Usage:"
+free -m
+
+echo "Top 5 processes:"
+ps aux --sort=-%mem | head -5
+
+echo "IP Address:"
+hostname -I
+
+echo "----- Completed -----"
